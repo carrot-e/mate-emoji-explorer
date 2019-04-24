257bae1c6bfc5f5e6e69fb570d4eb8c75faf4f76
carrot-e <elenalistratova11@gmail.com>
2019-04-24 18:19:17 +0300

    Implement basic version

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,51 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>emoji Explorer</h1>
+    <input v-model="searchValue" />
+    <ul>
+      <li v-for="emoji in shownEmojis" :key="emoji.no">
+        {{ emoji.char }}{{ emoji.name }}
+      </li>
+
+      <li v-if="shownEmojis.length === 0">
+        Nothing there...
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      searchValue: '',
+      list: []
+    };
+  },
+  computed: {
+    shownEmojis() {
+      let filteredList = this.list;
+
+      if (this.searchValue !== '') {
+        filteredList = this.list.filter((emoji) => {
+          return emoji.keywords.includes(this.searchValue.toLowerCase());
+        });
+      }
+
+      return filteredList.slice(0, 20);
+    },
+  },
+  methods: {
+    requestList() {
+      return fetch('https://raw.githubusercontent.com/amio/emoji.json/master/emoji.json')
+        .then(response => response.json())
+        .then(data => this.list = data);
+    }
+  },
+  created() {
+    this.requestList();
   }
 }
 </script>
